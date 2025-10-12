6a805fa1bfff42d5043a5538f668615c29ef1c5d
cpr4z <matvey.agarkov02@gmail.com>
2025-10-12 23:12:16 +0300

    one more small fix lab3

--- Labs/lab3/graph/depth_first_search.cpp
+++ Labs/lab3/graph/depth_first_search.cpp
@@ -52,9 +52,7 @@ void DepthFirstSearch::init(const HyperGraph& graph,
     m_forbidden_rules.clear();
     m_rules.clear();
 
-//    auto rules = graph.rules();
     m_rules = graph.rules();
-
     m_closed_vertices.insert(in_node_arr.begin(), in_node_arr.end());
     m_opened_vertices.push(goal_node);
 }
